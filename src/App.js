c2847ffbab915ad317826ee489e8306e4e7c5aac
misael-tup <misabul@comunidad.unam.edu>
2023-12-03 20:58:47 -0600

    Agregue contacto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import NavBar from './components/navbar/NavBar';
+import "./App.css";
+import NavBar from "./components/navbar/NavBar";
+import Contacto from "./pages/contacto/Contacto.jsx";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Contacto />
     </div>
   );
 }
