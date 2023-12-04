6a965b41b9c12b3a90ab66483513e9bc61f74125
misael-tup <misabul@comunidad.unam.edu>
2023-12-04 16:38:28 -0600

    Regresar el index a como estaba

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <BrowserRouter basename='/3D-Pastor'>
-        <App />
-      </BrowserRouter>
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
