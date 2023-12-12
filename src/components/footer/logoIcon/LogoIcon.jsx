36e2dfcf4f568bb93c86a1c394402b1ab9a24e1d
Mirta Tapia <mirta.atd@gmail.com>
2023-12-12 15:51:10 -0600

    Agregué un target=_blank para que los links a redes sociales se abran en pestañas nuevas y no en la misma de nuestra página

--- src/components/footer/logoIcon/LogoIcon.jsx
+++ src/components/footer/logoIcon/LogoIcon.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const LogoIcon = (prop) => {
   return (
     <div className="col-2 logo-container-footer">
-      <Link to={prop.to}>
+      <Link to={prop.to} target="_blank">
         <div className="card text-center bg-transparent border-0">
           <img className={"card-img-top " + prop.className} src={prop.src} alt={prop.alt} />
           <div className="card-body px-0">
