74edba8f1235032778112d4e8b4e4f541bef8573
Mirta Tapia <mirta.atd@gmail.com>
2023-12-13 00:46:19 -0600

    Seguí trabajando en el filtro, aún no funciona

--- src/components/productos-contenido/FiltroTarjetaProducto.jsx
+++ src/components/productos-contenido/FiltroTarjetaProducto.jsx
@@ -12,20 +12,26 @@ export default function FiltroTarjetaProducto({ nombreDelFiltro }) {
     setProducts(pokemonesData);
   }, []);
 
+  function filtrarPorCategoria(nombreDelFiltro) {
+    products.map((product, index) => {
+      if (product.category == nombreDelFiltro) {
+        <TarjetaProductos
+          key={index}
+          src={require("../../img/productos/" + product.image)}
+          alt={product.title}
+          nombreProducto={product.title}
+          precio={"$" + (product.price).toFixed(2) + " MXN"}
+        />
+      }
+    })
+  }
+
   return (
     <>
       <section>
         <div className="fondo container">
           <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
-            {products.map((product, index) => (
-              <TarjetaProductos
-                key={index}
-                src={require("../../img/productos/" + product.image)}
-                alt={product.title}
-                nombreProducto={product.title}
-                precio={"$" + (product.price).toFixed(2) + " MXN"}
-              />
-            ))}
+            {filtrarPorCategoria(nombreDelFiltro)}
           </div>
         </div>
       </section>
