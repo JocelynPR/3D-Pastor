ec5b3b12ab3f71185d97149d1d3989c4136f38d0
misael-tup <misabul@comunidad.unam.edu>
2023-12-13 17:31:09 -0600

    Guardar informacion en localstorage

--- src/pages/carrito/Carrito.jsx
+++ src/pages/carrito/Carrito.jsx
@@ -1,35 +1,55 @@
 // Carrito.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/buyButton/buyButton.css";
 import "../../styles/carrito/carrito.css";
 import BuyButton from "../../components/buyButton/BuyButton.jsx";
 import TablaCarrito from "./TablaCarrito.jsx"
 import CleanCardButton from "../../components/clearCardButton/ClearCardButton.jsx";
+import pokemonesData from "../../json/productos.json";
 
 const Carrito = () => {
 
-    const productos = [
-        { nombre: 'Pokémon Coup', precio: 250.00, cantidad: 2, subtotal: 500.00 },
-        { nombre: 'Pokélate', precio: 100.00, cantidad: 1, subtotal: 100.00 },
-        // Agrega más productos según sea necesario
-    ];
+    /*     const productos = [
+            { nombre: 'Pokémon Coup', precio: 250.00, cantidad: 2, subtotal: 500.00 },
+            { nombre: 'Pokélate', precio: 100.00, cantidad: 1, subtotal: 100.00 },
+            // Agrega más productos según sea necesario
+        ]; */
+
+    const [productos, setProducts] = useState([]);
+    const [carrito, setCarrito] = useState([]);
+
+    useEffect(() => {
+        setProducts(pokemonesData);
+    }, []);
 
 
-    const calcularTotalPorProducto = (producto) => {
-        return (producto.precio * producto.cantidad);
-    };
 
-    const calcularTotalGeneral = () => {
-        let totalGeneral = 0;
-        productos.forEach((producto) => {
-            totalGeneral += calcularTotalPorProducto(producto);
-        });
-        return totalGeneral.toFixed(2);
-    };
+    const sendFetchedDataToLocalStorage = (productos) => {
+        const data = (productos);
+        localStorage.setItem("data", JSON.stringify(data));
+    }
 
+    console.log(sendFetchedDataToLocalStorage(productos));
+
+
+
+
+    /* 
+        const calcularTotalPorProducto = (producto) => {
+            return (producto.precio * producto.cantidad);
+        };
+    
+        const calcularTotalGeneral = () => {
+            let totalGeneral = 0;
+            productos.forEach((producto) => {
+                totalGeneral += calcularTotalPorProducto(producto);
+            });
+            return totalGeneral.toFixed(2);
+        };
+     */
     return (
         <>
-            <main id="main-carrito-contenido">
+            {/*             <main id="main-carrito-contenido">
                 <section className="pt-5 pb-3 text-center container">
                     <h1 className="h1-productos-contenido fw-light"><strong>Mi carrito</strong></h1>
                 </section>
@@ -47,7 +67,7 @@ const Carrito = () => {
                         <CleanCardButton />
                     </div>
                 </section>
-            </main>
+            </main> */}
         </>
 
     );
