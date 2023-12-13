d5980c2f6b09d0f62ede93e9500ca00c9a131dc9
Jocelyn Peña <jocelyn.p.reyes332b@gmail.com>
2023-12-13 04:06:31 -0600

    Se agrega cambios de tarjeta

--- src/pages/sobreNosotros/SobreNosotros.jsx
+++ src/pages/sobreNosotros/SobreNosotros.jsx
@@ -1,6 +1,5 @@
 import "../../styles/sobre-nosotros/SobreNosotros.css";
 import yaz from "../../img/fotosPerfil/imagen_1.jpeg";
-import itzel from "../../img/fotosPerfil/imagen_2.jpeg";
 import tony from "../../img/fotosPerfil/imagen_3.jpeg";
 import jorge from "../../img/fotosPerfil/imagen_4.jpeg";
 import jocelyn from "../../img/fotosPerfil/imagen_5.jpeg";
