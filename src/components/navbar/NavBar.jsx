e6d72627fd8a4d48f9143606590a1a0e420469aa
Jorge Ramirez <jorgermzc16@gmail.com>
2023-12-13 00:47:12 -0600

    navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -17,38 +17,44 @@ import '../../styles/navbar/navBar.css';
 const NavBar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg custom-navbar">
-        <div className="navbar-brand">
-          <CartIcon src={pokemon_place_logo_with_border} to="/Home" className="pokemon-icon" />
-        </div>
-        <div className="mx-auto col-lg-4">
-          <form className="form-inline">
-            <div id="search-wrapper">
-              <input type="search" id="search" placeholder="Buscar..." />
-              <i className="fa-search">
-                <CartIcon src={search} className="search-icon" id="searchIcon" />
-              </i>
+      <nav className="navbar ">
+        <nav className="container-fluid custom-navbar">
+          <div className="row">
+              <div className="col">
+                  <CartIcon src={pokemon_place_logo_with_border} to="/Home" className="pokemon-icon " />
+              </div>
+              <div className="col">
+                <Link to="/LogIn" className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
+                  <span id="log-in">Log In</span>
+                  <CartIcon src={person_logo} className="svg-cart-icon" id="personIcon" />
+                </Link>
+              </div>
+              <div className="col">
+                <CartIcon src={shopping_cart} to="/Carrito" alt="Carrito de compras" className="svg-cart-icon" id="cartIcon" />
+              </div>
+              <div className="col">
+                <form class="container-fluid searcher">
+                  <div class="input-group">
+                    <span class="input-group-text " id="basic-addon1">
+                      <i>
+                        <CartIcon src={search} className="search-icon" id="searcherIcon" />
+                      </i>
+                    </span>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                  </div>
+                </form>
+              </div>
             </div>
-          </form>
-        </div>
-        <div className="d-flex justify-content-end align-items-center">
-          <div className="container m-3">
-            <Link to="/LogIn" className="row row-cols-2 row-cols-sm-2 row-cols-md-2 row-cols-lg-2 g-3">
-              <span id="log-in">Log In</span>
-              <CartIcon src={person_logo} className="svg-cart-icon" id="personIcon" />
-            </Link>
-          </div>
-          <CartIcon src={shopping_cart} to="/Carrito" alt="Carrito de compras" className="svg-cart-icon" id="cartIcon" />
-        </div>
+          </nav>
+        <nav id="category-bar-navbar" className="container-fluid cols-2 cols-sm-3 cols-md-4 cols-lg-6 g-2">
+          <CartIcon src={dices} to="/ProductsOficial" className="category-icon" description="Producto Oficial" />
+          <CartIcon src={key} to="ProductsAccesorios" className="category-icon" description="Accesorios" />
+          <CartIcon src={bunny} to="/ProductsJuguetes" className="category-icon" description="Juguetes" />
+          <CartIcon src={cake} to="/ProductsPostres" className="category-icon" description="Postres" />
+          <CartIcon src={table} to="/ProductsJuegos" className="category-icon" description="Juegos de Mesa" />
+          <CartIcon src={pokeball} to="/ProductsOtros" className="category-icon" description="Otros" />
+        </nav>
       </nav>
-      <div id="category-bar-navbar" className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-3 category-bar">
-        <CartIcon src={dices} to="/ProductsOficial" className="category-icon" description="Producto Oficial" />
-        <CartIcon src={key} to="ProductsAccesorios" className="category-icon" description="Accesorios" />
-        <CartIcon src={bunny} to="/ProductsJuguetes" className="category-icon" description="Juguetes" />
-        <CartIcon src={cake} to="/ProductsPostres" className="category-icon" description="Postres" />
-        <CartIcon src={table} to="/ProductsJuegos" className="category-icon" description="Juegos de Mesa" />
-        <CartIcon src={pokeball} to="/ProductsOtros" className="category-icon" description="Otros" />
-      </div>
     </>
   );
 };
