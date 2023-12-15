7d056b3ca25cc9b58007288e21c537bf247e87f0
Jorge Ramirez <jorgermzc16@gmail.com>
2023-12-15 17:40:59 -0600

    login

--- src/pages/logIn/LogIn.jsx
+++ src/pages/logIn/LogIn.jsx
@@ -1,6 +1,29 @@
+import "../../styles/logIn/LogIn.css"
+import LogInDesktop from "./LogInDesktop"
+import LogInMobile from "./LogInMobile"
+
 
 export default function LogIn() {
+  /*const localStorageUser = "userData";
+  const localStoragePassword = "passData";
+
+  const getUsers = () =>{
+    const usersData = localStorage.getItem(localStorageUser)
+
+    if (usersData){
+      return;
+    }else{
+      return;
+    }
+  }*/
   return (
-    <div>LogIn</div>
-  )
+    <>
+      <div className="d-none d-lg-block">
+        < LogInDesktop/>
+      </div >
+      <div className="d-lg-none">
+        <LogInMobile />
+      </div>
+    </>
+  );
 }
